c37626d8e3e1ffa643bba6f02ba014a05e7bb046
oscar3873 <47361577+oscar3873@users.noreply.github.com>
2022-05-21 19:53:49 -0300

    refactored header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TerminalComponent } from './components/terminal/terminal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TerminalComponent
   ],
   imports: [
     BrowserModule
