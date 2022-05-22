3cfeb1add20ce40cb543a5213ce746ef4e58735a
oscar3873 <47361577+oscar3873@users.noreply.github.com>
2022-05-22 02:20:56 -0300

    added terminal

--- src/app/components/terminal/terminal.component.ts
+++ src/app/components/terminal/terminal.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TerminalComponent implements OnInit {
 
+  text:string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+    this.init();
   }
+
+  async init () {
+
+    await this.dormir(1000);
+    await this.escribir("Hola, ");
+
+    while(true){
+      await this.escribir("Me llamo Oscar Vargas");
+      await this.dormir(2000);
+      await this.borrar();
+      await this.escribir("Y soy Desarrollador FullStack JR.");
+      await this.dormir(2000);
+      await this.borrar();
+    }
+  }
+
+  async escribir(text:string){
+    for (let character of text) {
+      this.text += character
+      await this.dormir(this.typeInterval())
+    }
+  }
+
+  async borrar(){
+    for (let _character of this.text) {
+      this.text = this.text.slice(0, this.text.length -1)
+      await this.dormir(this.typeInterval())
+    }
+  }
+  async dormir(time: number){
+    return new Promise(resolve => setTimeout(resolve, time))
+  }
+
+  private typeInterval (): number {
+    const randomMs = 100 * Math.random();
+    return randomMs < 50 ? 30 : randomMs;
+  }
+  
 
 }
