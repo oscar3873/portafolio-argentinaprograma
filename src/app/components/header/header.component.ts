30bb68c7bcd7f37b4265b1721e5ad303aa42dcc2
oscar3873 <47361577+oscar3873@users.noreply.github.com>
2022-09-02 23:40:18 -0300

    crud(update) header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -11,15 +12,38 @@ export class HeaderComponent implements OnInit {
   @Input() onSession!: boolean;
   miPortafolio:any;
   modalOption = [false, false, false, false, false]
+  id!: number; 
 
-  constructor(private portfolioServ:PortfolioService) { }
+  public formGroup: FormGroup;
+
+  constructor(private portfolioServ:PortfolioService, private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      nombre: '',
+      titulo: '',
+      infoPersona:'',
+      ftoPerfil:''
+
+    });
+   }
 
   ngOnInit(): void {
     
     this.portfolioServ.obtenerDatosPersona()
     .subscribe((data)=>{
       this.miPortafolio =data[0];
+      this.formGroup.setValue({
+        nombre:this.miPortafolio.nombre,
+        titulo:this.miPortafolio.titulo,
+        infoPersona:this.miPortafolio.infoPersona,
+        ftoPerfil:this.miPortafolio.ftoPerfil
+      });
+      this.id = this.miPortafolio.id
     });
+
+  }
+
+  onSubmit(data:any){
+    this.editar(data)
   }
 
   logout(){
@@ -28,24 +52,30 @@ export class HeaderComponent implements OnInit {
   }
 
   openModal1(){
+    console.log(this.miPortafolio)
     this.modalOption[1] = true
+    return 1;
   }
   openModal2(){
     this.modalOption[2] = true
+    return 2;
   }
   openModal3(){
     this.modalOption[3] = true
+    return 3;
   }
   openModal4(){
     this.modalOption[4] = true
+    return 4;
   }
 
   closeModal(){
     this.modalOption = [false, false, false, false, false]
   }
 
-  editar(){
-    
+  editar(data:any){
+    this.portfolioServ.modificarDatosPersona(this.id,
+      data).subscribe()
   }
-
+  
 }
